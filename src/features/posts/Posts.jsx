4a84a67bf1912402beceaced1cfd4f845bc57634
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-10 22:39:56 +0530

    add basic card

--- src/features/posts/Posts.jsx
+++ src/features/posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { likeButtonPressed, loadPosts } from "./postSlice";
+import { MdFavorite } from "react-icons/md";
 
 export default function Posts() {
   const { posts, status, error } = useSelector((state) => {
@@ -17,24 +18,57 @@ export default function Posts() {
   }, [dispatch, status]);
 
   return (
-    <div className="mt-48">
-      <div>
-        {status === "loading" && <h2>loading...</h2>}
-        {status === "error" && <h2>something went wrong... {error}</h2>}
+    <div className="mt-20 mb-8 py-8 px-4">
+      <div className="flex flex-row gap-4  relative">
+        <div className="basis-2/3 ">
+          {status === "loading" && <h2>loading...</h2>}
+          {status === "error" && <h2>something went wrong... {error}</h2>}
+          {posts.map((post) => (
+            <article
+              key={post.postID}
+              className="border-1 shadow-lg rounded-2xl my-6"
+            >
+              <div className="caption"> {post.caption} </div>
+              <div className="likes">
+                {post.likes}{" "}
+                <button
+                  onClick={() => dispatch(likeButtonPressed(post.postID))}
+                >
+                  <span role="img" aria-label="like">
+                    ❤️
+                  </span>
+                </button>
+              </div>
+            </article>
+          ))}
 
-        {posts.map((post) => (
-          <article key={post.postID} className="post">
-            <div className="caption"> {post.caption} </div>
-            <div className="likes">
-              {post.likes}{" "}
-              <button onClick={() => dispatch(likeButtonPressed(post.postID))}>
-                <span role="img" aria-label="like">
-                  ❤️
-                </span>
-              </button>
+          <article className="border-1 shadow-lg rounded-2xl">
+            <div className="flex flex-col">
+              <div className="flex flex-row gap-2 p-4 items-center">
+                <img src="Images/logo2.png"></img>
+                <h1 className="font-medium text-lg">Madhushree</h1>
+              </div>
+              <div className="border-y-2">
+                <p className="m-4 text-2xl">Hey Coral Tube</p>
+                <img src="Images/sample1.jpg" alt="" />
+              </div>
+              <div className="p-4">
+                <div className="flex items-center gap-1 font-medium">
+                  <MdFavorite className="text-xl text-darkGray" /> 5 likes
+                </div>
+                <div className="flex gap-2">
+                  <h2 className="font-semibold">Madhushree</h2>
+                  <p>Heyyoo</p>
+                </div>
+                <p className="text-sm text-mediumGray">Feb 2, 2022</p>
+              </div>
             </div>
           </article>
-        ))}
+        </div>
+
+        <div className="basis-1/3 block overflow-y-auto">
+          <div className="right-5">Suggestions tab</div>
+        </div>
       </div>
     </div>
   );
