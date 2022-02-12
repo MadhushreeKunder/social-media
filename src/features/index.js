b15a5b169d896987f9936a560826289c3bd2e89b
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-12 22:34:44 +0530

    add index.js

---
+++ src/features/index.js
@@ -0,0 +1,8 @@
+export { Posts } from "./posts/posts";
+export { Profile } from "./profile/profile";
+export { PrivateRoute } from "./auth/routes/privateRoute";
+export { PublicRoute } from "./auth/routes/publicRoute";
+export { SignUp } from "./auth/signup/signup";
+export { setupAuthExceptionHandler } from "./auth/utils";
+export { setupAuthHeaderForServiceCalls } from "./auth/utils";
+export { Login } from "./auth/login/login";
