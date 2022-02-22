424602b0325bb0edab715061a7d9b060944c87c9
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-22 19:14:58 +0530

    fix style

--- src/features/users/suggestions.js
+++ src/features/users/suggestions.js
@@ -52,7 +52,7 @@ export const SuggestionsSection = () => {
               {/* <div userDetails={user} /> */}
               <Link to={`/profile/${user.userName}`}>
                 <div className="flex items-center gap-2">
-                  <img src={user.avatar} className="w-10 h-10 " />
+                  <img src={user.avatar} className="w-10 h-10 rounded-full" />
                   <div>
                     <p className="text-gray-700 font-medium">{user.userName}</p>
                     <p className="text-gray-400">{user.name}</p>
