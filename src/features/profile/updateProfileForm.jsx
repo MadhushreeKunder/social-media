f3e7f864a8a263f89c348c9064d857b6e1a98145
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-22 12:57:58 +0530

    refactor modal

--- src/features/profile/updateProfileForm.jsx
+++ src/features/profile/updateProfileForm.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { CLOUDINARY_PRESET, CLOUDINARY_URL } from "../utils";
 import { updateProfileButtonClicked, useProfile } from "./profileSlice";
-import { MdCameraAlt } from "react-icons/md";
+import { MdCameraAlt, MdClose } from "react-icons/md";
+import { UploadImage } from "../posts/uploadImage";
+import { Modal } from "../posts/Modal";
 
 export const UpdateProfileForm = () => {
   const {
@@ -64,54 +66,64 @@ export const UpdateProfileForm = () => {
     setLink(link);
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <button>Edit Profile</button>
+      <button
+        onClick={() => setIsOpen(true)}
+        className="border-2 border-gray-300 rounded-md px-2 text-sm"
+      >
+        Edit Profile
+      </button>
 
-      <div>
-        {/* overlay */}
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        I am modal
         <div>
-          <button top="0.25rem" left="1rem" size="lg" onClick={clearUpdates}>
-            x
-          </button>
-
           <div>
-            <img src={media?.url} alt={userName} />
-            <MdCameraAlt setMedia={setMedia} setToken={setToken} />
-          </div>
+            <button top="0.25rem" left="1rem" size="lg" onClick={clearUpdates}>
+              <MdClose />
+            </button>
 
-          <div>
-            <label> Name</label>
-            <input type="text" value={name} isReadOnly />
-          </div>
+            <div>
+              <img src={media?.url} alt={userName} />
+              <UploadImage setToken={setToken} setMedia={setMedia} />
+              {/* <MdCameraAlt setMedia={setMedia} setToken={setToken} /> */}
+            </div>
 
-          <div>
-            <label> Username</label>
-            <input type="text" value={userName} isReadOnly />
-          </div>
+            <div>
+              <label> Name</label>
+              <input type="text" value={name} readonly />
+            </div>
 
-          <div>
-            <label> Website</label>
+            <div>
+              <label> Username</label>
+              <input type="text" value={userName} readonly />
+            </div>
+
+            <div>
+              <label> Website</label>
+              <input
+                type="text"
+                value={inputLink}
+                onChange={(e) => setLink(e.target.value)}
+                placeholder="Website"
+              />
+            </div>
+
+            <div></div>
+            <label>Bio</label>
             <input
               type="text"
-              value={inputLink}
-              onChange={(e) => setLink(e.target.value)}
-              placeholder="Website"
+              value={inputBio}
+              onChange={(e) => setBio(e.target.value)}
+              placeholder="Write something about you.."
             />
+
+            <button onClick={updateProfile}>Update</button>
           </div>
-
-          <div></div>
-          <label>Bio</label>
-          <input
-            type="text"
-            value={inputBio}
-            onChange={(e) => setBio(e.target.value)}
-            placeholder="Write something about you.."
-          />
-
-          <button onClick={updateProfile}>Update</button>
         </div>
-      </div>
+      </Modal>
     </>
   );
 };
