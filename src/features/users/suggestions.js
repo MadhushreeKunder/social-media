226c9799a103b357590bb65d168960afc0fb0019
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-22 16:43:42 +0530

    fix styling

--- src/features/users/suggestions.js
+++ src/features/users/suggestions.js
@@ -27,7 +27,7 @@ export const SuggestionsSection = () => {
 
   return (
     <div>
-      <div className="sticky">
+      <div className="mt-5">
         <Link to={`/profile/${userName}`}>
           <div className="flex gap-3">
             <img
@@ -48,8 +48,17 @@ export const SuggestionsSection = () => {
             People to follow
           </p>
           {getFiveUnfollowedUser(users).map((user) => (
-            <div mb="0.8rem" key={user.userName}>
-              <div userDetails={user} />
+            <div className="my-4" key={user.userName}>
+              {/* <div userDetails={user} /> */}
+              <Link to={`/profile/${user.userName}`}>
+                <div className="flex items-center gap-2">
+                  <img src={user.avatar} className="w-10 h-10 " />
+                  <div>
+                    <p className="text-gray-700 font-medium">{user.userName}</p>
+                    <p className="text-gray-400">{user.name}</p>
+                  </div>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
