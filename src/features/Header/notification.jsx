f8d1bdfe517470c754d7da74db5dedb688a3b4a6
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-22 18:40:22 +0530

    fix notification

--- src/features/Header/notification.jsx
+++ src/features/Header/notification.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { MdNotifications } from "react-icons/md";
+import { Link } from "react-router-dom";
 import { useAuthentication } from "../auth/authenticationSlice";
 import { Modal } from "../posts";
 
@@ -16,17 +17,33 @@ export const Notification = () => {
 
       <Modal open={isOpen} onClose={() => setIsOpen(false)}>
         <div>
-            {notifications.length === 0 ? (
-                <p>
-                    No notifications.
-                </p>
-            ): (
-                notifications.map((activity) => (
-                <div>
-                        
+          {notifications.length === 0 ? (
+            <p>No notifications.</p>
+          ) : (
+            notifications.map((activity) => (
+              <div>
+                <Link to={`/profile/${activity.activityUserId.userName}`}>
+                  <div className="flex">
+                    <img
+                      src={activity.activityUserId.avatar}
+                      alt={activity.activityUserId.userName}
+                    />
+                    <div>
+                      <p>{activity.activityUserId.userName}</p>
                     </div>
-                ))
-            )}
+                    <p>
+                      {activity.activityTitle}
+                      <span>
+                        {activity?.likedPost?.caption &&
+                          `: ${activity?.likedPost?.caption}`}
+                      </span>
+                    </p>
+                    <p>{activity.time}</p>
+                  </div>
+                </Link>
+              </div>
+            ))
+          )}
         </div>
       </Modal>
     </>
